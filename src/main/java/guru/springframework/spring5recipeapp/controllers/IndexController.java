d1361b44fc05ab7114bf7a07e62197c48dfdb54a
gabbyka <gabbyka@192.168.0.215>
2018-02-15 21:21:55 +0100

    - chill with spring boot

--- src/main/java/guru/springframework/spring5recipeapp/controllers/IndexController.java
+++ src/main/java/guru/springframework/spring5recipeapp/controllers/IndexController.java
@@ -1,13 +1,24 @@
 package guru.springframework.spring5recipeapp.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import guru.springframework.spring5recipeapp.services.RandomService;
 
 @Controller
 public class IndexController {
+    
+    private final RandomService randomService;
+    
+    public IndexController(RandomService randomService) {
+        super();
+        this.randomService = randomService;
+    }
+
 
     @RequestMapping({"", "/", "/index"})
-    public String getIndexPage() {
+    public String getIndexPage(Model model) {
+        model.addAttribute("randomText", randomService.randomText());
         return "index";
     }
     

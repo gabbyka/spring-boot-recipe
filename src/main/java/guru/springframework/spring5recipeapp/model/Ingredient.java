bcc2917706c1174262c916c32d58d241879215be
gabbyka <gabbyka@192.168.0.215>
2018-02-19 21:15:31 +0100

    - finished the assignment

--- src/main/java/guru/springframework/spring5recipeapp/model/Ingredient.java
+++ src/main/java/guru/springframework/spring5recipeapp/model/Ingredient.java
@@ -2,10 +2,12 @@ package guru.springframework.spring5recipeapp.model;
 
 import java.math.BigDecimal;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 
 @Entity
 public class Ingredient {
@@ -19,6 +21,9 @@ public class Ingredient {
 
     @ManyToOne
     private Recipe recipe;
+    
+    @OneToOne(fetch = FetchType.EAGER)
+    private UnitOfMeasure unitOfMeasure;
 
 
     public Long getId() {
@@ -53,4 +58,12 @@ public class Ingredient {
         this.recipe = recipe;
     }
 
+    public UnitOfMeasure getUnitOfMeasure() {
+        return unitOfMeasure;
+    }
+
+    public void setUnitOfMeasure(UnitOfMeasure unitOfMeasure) {
+        this.unitOfMeasure = unitOfMeasure;
+    }
+    
 }

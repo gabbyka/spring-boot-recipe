5c1fa2b51efad26e1056163bbde441fd3da9fb36
gabbyka <gabbyka@192.168.0.215>
2018-02-28 20:55:21 +0100

    - soup recipe

--- src/main/java/guru/springframework/spring5recipeapp/loader/DatabaseLoader.java
+++ src/main/java/guru/springframework/spring5recipeapp/loader/DatabaseLoader.java
@@ -6,6 +6,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import guru.springframework.spring5recipeapp.model.Difficulity;
 import guru.springframework.spring5recipeapp.model.Ingredient;
+import guru.springframework.spring5recipeapp.model.Notes;
 import guru.springframework.spring5recipeapp.model.Recipe;
 import guru.springframework.spring5recipeapp.repositories.RecipeRepository;
 
@@ -22,26 +23,30 @@ public class DatabaseLoader implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        HashSet<Ingredient> ingerdients = new HashSet<>();
+        HashSet<Ingredient> hunSoupIngredients = new HashSet<>();
         
         Ingredient soupSaltIngerdient = new Ingredient();
         soupSaltIngerdient.setAmount(BigDecimal.valueOf(20L));
         soupSaltIngerdient.setDescription("salt");
         
-        ingerdients.add(soupSaltIngerdient);
+        hunSoupIngredients.add(soupSaltIngerdient);
         
         Ingredient soupMilkIngerdient = new Ingredient();
         soupMilkIngerdient.setAmount(BigDecimal.valueOf(20L));
         soupMilkIngerdient.setDescription("salt");
         
-        ingerdients.add(soupMilkIngerdient);
-        
+        hunSoupIngredients.add(soupMilkIngerdient);
         
         Recipe hungarianSoup = new Recipe();
         hungarianSoup.setDifficulity(Difficulity.EASY);
         hungarianSoup.setCookTime(60);
         hungarianSoup.setDescription("Good hungarian soup!");
-        //hungarianSoup.setIngredients(ingredients);
+        hungarianSoup.setIngredients(hunSoupIngredients);
+        
+        Notes hunSoupNote = new Notes();
+        hunSoupNote.setRecipeNotes("Very famous and very tasty soup from Hungary.");
+        
+        hungarianSoup.setNotes(hunSoupNote);
         
         recipeRepository.save(hungarianSoup);
         

3f5ce90466786b91f42fe6d34bc2121918ab38f3
gn <gn@star-trac.de>
2018-03-23 13:34:19 +0100

    - added command objects

---
+++ src/main/java/guru/springframework/spring5recipeapp/converters/NotesToNotesCommand.java
@@ -0,0 +1,28 @@
+package guru.springframework.spring5recipeapp.converters;
+
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Component;
+
+import guru.springframework.spring5recipeapp.commands.NotesCommand;
+import guru.springframework.spring5recipeapp.model.Notes;
+
+/**
+ * Created by jt on 6/21/17.
+ */
+@Component
+public class NotesToNotesCommand implements Converter<Notes, NotesCommand>{
+
+    @Nullable
+    @Override
+    public synchronized NotesCommand convert(Notes source) {
+        if (source == null) {
+            return null;
+        }
+
+        final NotesCommand notesCommand = new NotesCommand();
+        notesCommand.setId(source.getId());
+        notesCommand.setRecipeNotes(source.getRecipeNotes());
+        return notesCommand;
+    }
+}

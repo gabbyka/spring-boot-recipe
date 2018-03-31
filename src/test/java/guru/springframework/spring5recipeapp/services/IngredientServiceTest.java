5f0005bf2ca5e8a802aa4cc10188b53e0ad74f62
gabbyka <gabbyka@192.168.0.215>
2018-03-31 13:18:58 +0200

    - ingredient show improved

---
+++ src/test/java/guru/springframework/spring5recipeapp/services/IngredientServiceTest.java
@@ -0,0 +1,23 @@
+package guru.springframework.spring5recipeapp.services;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class IngredientServiceTest {
+
+    @Test
+    public void testGetIngredientById() {
+        //fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetIngredientCommandById() {
+        //fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetIngredientCommandByRecipeIdAndIngredientId() {
+        //fail("Not yet implemented");
+    }
+
+}

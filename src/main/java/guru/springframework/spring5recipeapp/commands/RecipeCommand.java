ad0b14336a05b26384c250f11575e3707a93a32b
gabbyka <gabbyka@192.168.0.214>
2018-04-14 16:18:03 +0200

    - added validation

--- src/main/java/guru/springframework/spring5recipeapp/commands/RecipeCommand.java
+++ src/main/java/guru/springframework/spring5recipeapp/commands/RecipeCommand.java
@@ -2,7 +2,11 @@ package guru.springframework.spring5recipeapp.commands;
 
 import java.util.HashSet;
 import java.util.Set;
-
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import org.hibernate.validator.constraints.URL;
 import guru.springframework.spring5recipeapp.model.Difficulity;
 
 /**
@@ -10,12 +14,28 @@ import guru.springframework.spring5recipeapp.model.Difficulity;
  */
 public class RecipeCommand {
     private Long id;
+    
+    @NotBlank
+    @Size(min = 3, max = 255)
     private String description;
+    
+    @Min(1)
+    @Max(999)
     private Integer prepTime;
+    
+    @Min(1)
+    @Max(999)
     private Integer cookTime;
+    
+    @Min(1)
+    @Max(100)
     private Integer servings;
     private String source;
+    
+    @URL
     private String url;
+    
+    @NotBlank
     private String directions;
     private Set<IngredientCommand> ingredients = new HashSet<>();
     private Difficulity difficulty;
--- src/main/java/guru/springframework/spring5recipeapp/controllers/RecipeController.java
+++ src/main/java/guru/springframework/spring5recipeapp/controllers/RecipeController.java
@@ -1,8 +1,12 @@
 package guru.springframework.spring5recipeapp.controllers;
 
+import javax.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -16,25 +20,28 @@ import guru.springframework.spring5recipeapp.services.RecipeService;
 
 @Controller
 public class RecipeController {
+    private static final Logger log = LoggerFactory.getLogger(RecipeController.class);
+    
+    private static final String RECIPE_RECIPEFORM_URL = "recipe/recipeform";
     
     private final RecipeService recipeService;
 
     public RecipeController(RecipeService recipeService) {
         this.recipeService = recipeService;
     }
-    
+
     @GetMapping("/recipe/{id}/show")
     public String getRecipe(@PathVariable String id, Model model) {
         model.addAttribute("recipe", recipeService.getRecipeById(new Long(id)));
         return "recipe/show";
     }
-    
+
     @GetMapping("/recipe/{id}/edit")
     public String editRecipe(@PathVariable String id, Model model) {
         model.addAttribute("recipe", recipeService.findCommandById(new Long(id)));
-        return "recipe/recipeform";
+        return RECIPE_RECIPEFORM_URL;
     }
-    
+
     @GetMapping("/recipe/{id}/delete")
     public String deleteRecipe(@PathVariable String id) {
         recipeService.deleteById(new Long(id));
@@ -44,11 +51,18 @@ public class RecipeController {
     @GetMapping("/recipe/new")
     public String newRecipe(Model model) {
         model.addAttribute("recipe", new RecipeCommand());
-        return "recipe/recipeform";
+        return RECIPE_RECIPEFORM_URL;
     }
-    
+
     @PostMapping("/recipe")
-    public String saveOrUpdate(@ModelAttribute RecipeCommand recipeCommand) {
+    public String saveOrUpdate(@Valid @ModelAttribute("recipe") RecipeCommand recipeCommand,
+            BindingResult bindingResult) {
+        if(bindingResult.hasErrors()) {
+            bindingResult.getAllErrors().forEach(objectError -> {
+                log.info(objectError.toString());
+            });
+            return RECIPE_RECIPEFORM_URL;
+        }
         RecipeCommand savedCommand = recipeService.saveRecipeCommand(recipeCommand);
         return "redirect:/recipe/" + savedCommand.getId() + "/show";
     }
